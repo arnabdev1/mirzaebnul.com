df3d0a22091b3901d24daaf43cd10b06750555a4
Arnab <official.arnabdev@gmail.com>
2026-06-02 14:45:01 -0500

    Deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  swcMinify: false,
   images: {
+    unoptimized: true,
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
